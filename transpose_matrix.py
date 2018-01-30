7daf72c7692e060a77735e8d20a16eaa5da9121c
Jasjyot <jasjyot98@gmail.com>
2018-01-30 12:35:07 +0530

    Create transpose_matrix.py

---
+++ transpose_matrix.py
@@ -0,0 +1,11 @@
+lst=[[1,2,3,4],
+     [5,6,7,8],
+     [9,10,11,12]]
+transpose=[]
+
+for i in range(4):
+    l=[]
+    for j in range(3):
+        l.append(lst[j][i])
+    transpose.append(l)
+print(transpose)
